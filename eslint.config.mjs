3a37e56e526c2d5d03e7a4735d05d9e08b92259b
Michael KT <mktanzil06@gmail.com>
2025-01-04 12:44:10 +0700

    ESlint & Prettier Setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,86 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import importPlugin from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: { root: true },
+  ignorePatterns: ["components/ui/**"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "no-undef": "off",
+      },
+    },
+  ],
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      },
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            {
+              pattern: "next",
+              group: "external",
+            },
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+            {
+              pattern: "./**/*.css",
+              group: "index",
+              position: "after",
+            },
+          ],
+          "newlines-between": "always",
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "eslint:recommended",
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
+  ...compat.plugins("promise", "n"),
 ];
 
 export default eslintConfig;

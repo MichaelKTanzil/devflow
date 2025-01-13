514243202ffec7e86e2f466fdfe5e84c53b287ee
Michael KT <mktanzil06@gmail.com>
2025-01-13 23:29:06 +0700

    Implementing Filter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,88 @@
+import Link from "next/link";
 import React from "react";
 
-import { auth } from "@/auth";
+import HomeFilter from "@/components/filter/HomeFilter";
+import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
-const Home = async () => {
-  const session = await auth();
-  console.log(session);
+const questions = [
+  {
+    _id: "1",
+    title: "What is the best way to learn React?",
+    description:
+      "I am trying to learn React and I am looking for the best way to learn it. Can someone help me?",
+    tags: [{ id: "1", name: "React" }],
+    author: {
+      _id: "1",
+      name: "John Doe",
+    },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "How to use map in javascript?",
+    description:
+      "I am trying to use map in javascript but I am facing some issues. Can someone help me?",
+    tags: [{ _id: "1", name: "Javascript" }],
+    author: {
+      _id: "2",
+      name: "Jane Doe",
+    },
+    upvotes: 15,
+    answers: 7,
+    views: 120,
+    createdAt: new Date(),
+  },
+];
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+const Home = async ({ searchParams }: SearchParams) => {
+  const { query = "", filter = "" } = await searchParams;
+
+  const filteredQuestions = questions.filter((question) => {
+    const matchesQuery = question.title
+      .toLowerCase()
+      .includes(query?.toLowerCase());
+    const matchesFilter = filter
+      ? question.tags.some(
+          (tag) => tag.name.toLowerCase() === filter.toLowerCase()
+        )
+      : true;
+    return matchesQuery && matchesFilter;
+  });
+
   return (
     <>
-      <div>Home</div>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search for Questions Here..."
+          otherClasses="flex-1"
+        />
+      </section>
+      <HomeFilter />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
+      </div>
     </>
   );
 };
